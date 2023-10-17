00b75f01e0fa194fcb1123234cf5a3ed98485614
Zachariah-W <zachwangs@gmail.com>
2023-10-16 23:25:20 -0700

    Delete and Edit

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-let count = 0;
-let data = [50];
+let data = [];
+
+function addElt(){
+    document.getElementById("showTask").innerHTML = "";
+    const showElts = document.getElementById("showTask");
+    data.forEach((element,i) => {
+        let taskList = document.createElement("div")
+        taskList.setAttribute("class", "taskContainer");
+        let checkTask = document.createElement("button");
+        checkTask.setAttribute("class","checkButtons");
+        let editTask = document.createElement("button");
+        editTask.setAttribute("class", "editButtons");
+        showElts.appendChild(taskList);
+        taskList.innerText = "Task " + (i+1) + ": " + element;
+        checkTask.innerText = "Check Off";
+        editTask.innerText = "Edit Task"
+        taskList.appendChild(checkTask);
+        taskList.appendChild(editTask);
+        checkTask.onclick = () => deleteTask(i);
+        editTask.onclick = () => editElt(i);
+    });
+}
 
 function addTask(){
-    let userInput = prompt("Enter Task:");
-    if(userInput != null){
-        console.log(userInput);
-        data[count] = userInput;
-        count ++;
-    };
-    console.log(data)
-    let showElts = document.getElementById("showTask");
-    let taskList = document.createElement("div")
-    taskList.setAttribute("class", "taskContainer");
-    let checkTask = document.createElement("button");
-    checkTask.setAttribute("class","checkButtons");
-    showElts.appendChild(taskList);
-    taskList.innerText = "- " + data[count - 1];
-    checkTask.innerText = "Click to check off";
-    taskList.appendChild(checkTask);
+    let newUserInput = prompt("Enter Task:");
+    newUserInput != "" && data.push(newUserInput); // The first part is the if condition, and if the condition is true, the command after && will run 
+    console.log(data);
+    addElt();
+}
+
+function deleteTask(i){
+    data.splice(i,1);
+    addElt();
+}
+
+function editElt(i){
+    let editUserInput = prompt("Eenter New Task:");
+    editUserInput != "" && (data[i] = editUserInput);
+    addElt();
 }
