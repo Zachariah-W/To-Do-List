2dd1380842fdc845ea16a17644cd9d6876f424a7
Zachariah-W <zachwangs@gmail.com>
2023-10-17 21:02:08 -0700

    Adding Due Date

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let data = [];
+let date = [];
 
 function addElt(){
     document.getElementById("showTask").innerHTML = "";
@@ -10,31 +11,45 @@ function addElt(){
         checkTask.setAttribute("class","checkButtons");
         let editTask = document.createElement("button");
         editTask.setAttribute("class", "editButtons");
+        let editDate = document.createElement("button");
+        editDate.setAttribute("class", "dateButton");
         showElts.appendChild(taskList);
         taskList.innerText = "Task " + (i+1) + ": " + element;
         checkTask.innerText = "Check Off";
         editTask.innerText = "Edit Task"
+        editDate.innerText = "Due: " + date[i];
         taskList.appendChild(checkTask);
         taskList.appendChild(editTask);
+        taskList.appendChild(editDate);
         checkTask.onclick = () => deleteTask(i);
         editTask.onclick = () => editElt(i);
+        editDate.onclick = () => changeDate(i);
     });
 }
 
 function addTask(){
-    let newUserInput = prompt("Enter Task:");
-    newUserInput != "" && data.push(newUserInput); // The first part is the if condition, and if the condition is true, the command after && will run 
+    let newUiTask = prompt("Enter Task:");
+    newUiTask != "" && data.push(newUiTask)
+    let newUiDate = prompt("Enter Due Date:");
+    date.push(newUiDate);
     console.log(data);
+    console.log(date);
     addElt();
 }
 
 function deleteTask(i){
     data.splice(i,1);
+    date.splice(i,1);
+    addElt();
+}
+function changeDate(i){
+    let uiNewDate = prompt("Enter New Date:");
+    uiNewDate != "" && (date[i] = uiNewDate);
     addElt();
 }
 
 function editElt(i){
-    let editUserInput = prompt("Eenter New Task:");
-    editUserInput != "" && (data[i] = editUserInput);
+    let editUi = prompt("Eenter New Task:");
+    editUi != "" && (data[i] = editUi);
     addElt();
 }
