0bac353608ff2f49b1d8dc80e0eb1ca1afdb670b
Zachariah-W <zachwangs@gmail.com>
2023-10-18 22:36:19 -0700

    Sort by month

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let data = [];
-let date = [];
 
 function addElt(){
     document.getElementById("showTask").innerHTML = "";
     const showElts = document.getElementById("showTask");
+    console.log(data.sort(function(a, b){return a.date.split("")[0] - b.date.split("")[0]}))
     data.forEach((element,i) => {
         let taskList = document.createElement("div")
         taskList.setAttribute("class", "taskContainer");
@@ -14,42 +14,42 @@ function addElt(){
         let editDate = document.createElement("button");
         editDate.setAttribute("class", "dateButton");
         showElts.appendChild(taskList);
-        taskList.innerText = "Task " + (i+1) + ": " + element;
+        taskList.innerText = "Task " + (i+1) + ": " + element.task;
         checkTask.innerText = "Check Off";
         editTask.innerText = "Edit Task"
-        editDate.innerText = "Due: " + date[i];
+        editDate.innerText = "Due: " + element.date;
         taskList.appendChild(checkTask);
         taskList.appendChild(editTask);
         taskList.appendChild(editDate);
         checkTask.onclick = () => deleteTask(i);
-        editTask.onclick = () => editElt(i);
-        editDate.onclick = () => changeDate(i);
+        editTask.onclick = () => editElt(element, i);
+        editDate.onclick = () => changeDate(element, i);
     });
 }
 
 function addTask(){
+    let tempObj = [{task:"", date:""}]
     let newUiTask = prompt("Enter Task:");
-    newUiTask != "" && data.push(newUiTask)
-    let newUiDate = prompt("Enter Due Date:");
-    date.push(newUiDate);
+    if(newUiTask != "") tempObj.task = newUiTask;
+    let newUiDate = prompt("Enter Due Date(Month/Date Ex:5/18):");
+    if(newUiDate != "") tempObj.date = newUiDate;
+    data.push(tempObj);
     console.log(data);
-    console.log(date);
     addElt();
 }
 
 function deleteTask(i){
     data.splice(i,1);
-    date.splice(i,1);
     addElt();
 }
-function changeDate(i){
+function changeDate(element, i){
     let uiNewDate = prompt("Enter New Date:");
-    uiNewDate != "" && (date[i] = uiNewDate);
+    uiNewDate != "" && (element.date = uiNewDate);
     addElt();
 }
 
-function editElt(i){
+function editElt(element, i){
     let editUi = prompt("Eenter New Task:");
-    editUi != "" && (data[i] = editUi);
+    editUi != "" && (element.task = editUi);
     addElt();
 }
